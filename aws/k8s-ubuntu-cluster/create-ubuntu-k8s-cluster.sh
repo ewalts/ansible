b59dd0974804ec0ee2d72deea1684f67da744b71
Eric Walts <eric@richardwalts.com>
2023-11-09 15:05:24 -1000

    kubernetes deployment

--- aws/k8s-ubuntu-cluster/create-ubuntu-k8s-cluster.sh
+++ aws/k8s-ubuntu-cluster/create-ubuntu-k8s-cluster.sh
@@ -5,7 +5,7 @@
 #_#>
 
 rm -f aws-hosts
-
+rm /tmp/k8s_hosts
 echo '[local]
 localhost
 
@@ -19,26 +19,43 @@ my_ssh_key_path=$(cat current_play_vars.yml |grep my_ssh_key_path | awk '{print 
 echo "
 [k8s]" >> aws-hosts
 hosts=$(aws ec2 describe-instances --filters Name=tag:environment,Values=k8s|grep PublicIpAddress |awk '{print $2}')
+priv_hosts=$(aws ec2 describe-instances --filters Name=tag:environment,Values=k8s|grep PrivateIpAddress |awk '{print $2}' \
+ | sed -n '/^$/d;G;/^\(.*\n\).*\n\1$/d;H;P;a\ ')
 
+###> Create the aws-hosts file for ansible local with public Ip addresses
 c=0
 k8scp="\n[k8scp]\n"
 k8swn="[k8swn]\n"
-
 for h in $hosts;
 do
-        c=$((c+1))
         echo "$h  ansible_ssh_private_key_file=$my_ssh_key_path" >> aws-hosts
-        if [ $c = 1 ]
+        if [ $c = 0 ]
         then
+		echo "$h    k8s-cp" >> /tmp/pub_hosts
                 k8scp=${k8scp}"$h  ansible_ssh_private_key_file=$my_ssh_key_path\n"
-        elif [ $c > 1 ]
+        elif [ $c > 0 ]
         then
+		echo "$h    k8s-wn$c" >> /tmp/pub_hosts
                 k8swn=${k8swn}"$h  ansible_ssh_private_key_file=$my_ssh_key_path \n"
         fi
+	c=$((c+1))
 done
 echo -e $k8scp >> aws-hosts
 echo -e $k8swn >> aws-hosts
 
+###> Create the hosts file with private ips
+c=0
+for ph in $priv_hosts;
+do
+        if [ $c = 0 ]
+        then
+                echo "$ph    k8s-cp k8s-cp.rewbin.org" >> /tmp/k8s_hosts
+        elif [ $c > 0 ]
+        then
+                echo "$ph    k8s-wn$c k8s-wn$c.rewbin.org" >> /tmp/k8s_hosts
+        fi
+        c=$((c+1))
+done
 
 wait 45
 echo " Waiting for SSH on new instances."
